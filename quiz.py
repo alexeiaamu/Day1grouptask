239013cc7a7965bd1940bd8fe22b237f2e165f20
AlexeiAamu <alexeiaamu@gmail.com>
2024-10-21 15:30:41 +0300

    creating the game itself

--- quiz.py
+++ quiz.py
@@ -1,11 +1,21 @@
+score=0
 while True:
     start=int(input('Do you want to play the game? press 1 to start, 0 to exit'))
     if start==1:
-        continue
+        print('Great!')
     elif start==0:
         break
     else:
         print("invalid input")
+        
+    q1=int(input('What is 2 + 2?'))
+    if q1 == 4:
+        print('correct!')
+        score+=1
+    else:
+        print('incorrect')
+    break
 
 
 print('Thank you')
+print(f'your score was {score}')
